babf3c219dd050749d1311972daaf6f604264c95
Syond <syond94@gmail.com>
2020-07-23 01:27:51 -0300

    installed packages and modified configurations

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,13 @@
-module.exports = {
-    bail: true,
-    clearMocks: true,
-    collectCoverage: true,
-    collectCoverageFrom: ['src/app/**/*.js'],
-    coverageDirectory: "__tests__/coverage",
-    coverageReporters: [
-        "text",
-        "lcov",
-    ],
-    testEnvironment: "node",
-    testMatch: [
-        "**/__tests__/**/*.test.js?(x)",
-    ],
-}
+export default {
+  bail: true,
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ["src/app/**/*.js"],
+  coverageDirectory: "__tests__/coverage",
+  coverageReporters: ["text", "lcov"],
+  testEnvironment: "node",
+  testMatch: ["**/__tests__/**/*.test.js?(x)"],
+  transform: {
+    ".(js|jsx|ts|tsx)": "@sucrase/jest-plugin",
+  },
+};
