530f4eefc6197c3275cb7a9e8e354147faaced95
Syond <syond94@gmail.com>
2020-07-23 01:30:41 -0300

    test: should create new user - PASS

---
+++ __tests__/integration/user.test.js
@@ -0,0 +1,27 @@
+import request from "supertest";
+import app from "../../src/app.js";
+
+let users;
+
+describe("Users tests", () => {
+  beforeEach(() => {
+    users = [
+      {
+        name: "Fernando Lopes",
+        age: "22",
+        email: "fernando.lopes@tdd.com",
+      },
+      {
+        name: "Adriana Lisboa",
+        age: "27",
+        email: "adriana.lisboa@tdd.com",
+      },
+    ];
+  });
+
+  it("should create new user", async () => {
+    const response = await request(app).post("/users").send(users[0]);
+
+    expect(response.body).toHaveProperty('name');
+  });
+});
