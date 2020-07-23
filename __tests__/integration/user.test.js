90a564a1cc3dccb6d4dcd1cdb9d874e4b3b612ba
Syond <syond94@gmail.com>
2020-07-23 14:47:51 -0300

    test: should not create new user with same email - PASS

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,5 +1,6 @@
 import request from "supertest";
 import app from "../../src/app.js";
+import database from "../../src/database";
 
 let users;
 
@@ -17,17 +18,49 @@ describe("Users tests", () => {
         email: "adriana.lisboa@tdd.com",
       },
     ];
+
+    database = [];
   });
 
   it("should create new user", async () => {
     const response = await request(app).post("/users").send(users[0]);
 
-    expect(response.body).toHaveProperty('name');
+    expect(response.body).toHaveProperty("name");
   });
-  
+
   it("should return status 201 when create new user", async () => {
-    const response = await request(app).post('/users').send(users[0]);
+    const response = await request(app).post("/users").send(users[0]);
 
     expect(response.status).toBe(201);
+  });
+
+  it("should not create new user with same email", async () => {
+    await request(app)
+      .post("/users")
+      .send({ ...users[0], name: "Adriano Souza" });
+
+    const response = await request(app).post("/users").send(users[0]);
+
+    expect(response.status).toBe(400);
+  });
+
+  it("should update user", async () => {
+    //TODO
+  });
+
+  it("should not update user witch not exist", async () => {
+    //TODO
+  });
+
+  it("should delete user", async () => {
+    //TODO
+  });
+
+  it("should return status 204 when delete user", async () => {
+    //TODO
+  });
+
+  it("should not delete user witch not exist", async () => {
+    //TODO
   });
 });
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,10 +1,18 @@
+import database from "../../database";
+
 class UserController {
   store(request, response) {
     const user = request.body;
 
-    if (!user) {
-      response.json({ error: "Something wrong" });
+    if (!user) response.json({ error: "Something went wrong" });
+
+    const checkUser = database.find((u) => u.email == user.email);
+
+    if (checkUser) {
+      response.status(400).json({ error: "Email already in use." });
     } else {
+      database.push(user);
+
       response.status(201).json(user);
     }
   }
